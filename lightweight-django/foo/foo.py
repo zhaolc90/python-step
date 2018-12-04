0113bbcc32c9f3ed60395e1b065f48496f4bba69
c <ytmssr@outlook.com>
2018-12-04 22:22:19 +0800

    http://localhost:8000/image/300x120/  占位图片

--- lightweight-django/foo/foo.py
+++ lightweight-django/foo/foo.py
@@ -1,11 +1,13 @@
 # django==1.7.0
 import sys
 import os
+from io import BytesIO
+from PIL import Image
 
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseBadRequest
+from django import forms
 from django.conf import settings
 DEBUG = os.environ.get('DEBUG', 'on') ==  'on'
 SECRET_KEY = os.environ.get('SECRET_KEY', 'c+baojq!^z-2vw#e^ep=&1!r#acaodq4rr3d0lb#&kw9vbyxhx')
@@ -22,11 +24,33 @@ settings.configure(
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ),
 )
+class ImageForm(forms.Form):
+    height= forms.IntegerField(min_value=1,max_value=2000)
+    width= forms.IntegerField(min_value=1,max_value=2000)
+    def generate(self, image_format='PNG'):
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image=Image.new('RGB', (width,height))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
+
+def placeholder(request, width, height):
+    print(width)
+    form = ImageForm({'height': height, 'width': width})
+    if form.is_valid():
+        image=form.generate()
+        return HttpResponse(image,content_type='image/png' )
+    else:
+        return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
     return HttpResponse('Hello World')
 
 urlpatterns = (
+    url(r'^image/(?P<width>[0-9]+)x(?P<height>[0-9]+)/$', placeholder,
+        name='placeholder'),
     url(r'^$', index),
 )
 
