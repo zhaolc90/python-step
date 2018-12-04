f25e338326ceebd512dfa33cdc44e090071a6b09
c <ytmssr@outlook.com>
2018-12-04 21:00:49 +0800

    启动配置 参数修改

--- lightweight-django/hello.py
+++ lightweight-django/hello.py
@@ -1,14 +1,20 @@
 # django==1.7.0
 import sys
+import os
 
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse
 
 from django.conf import settings
+DEBUG = os.environ.get('DEBUG', 'on') ==  'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(32))
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
 settings.configure(
-    DEBUG=True,
-    SECRET_KEY='thisisthesecretkey',
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
     ROOT_URLCONF=__name__,
     MIDDLEWARE_CLASSES=(
         'django.middleware.common.CommonMiddleware',
