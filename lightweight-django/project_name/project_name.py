5f87ffa7540956ca63cd7714e1b9e6af1ab6ac26
c <ytmssr@outlook.com>
2018-12-04 21:36:55 +0800

    django-admin startproject foo --template=project_name

---
+++ lightweight-django/project_name/project_name.py
@@ -0,0 +1,38 @@
+# django==1.7.0
+import sys
+import os
+
+from django.conf.urls import url
+from django.core.wsgi import get_wsgi_application
+from django.http import HttpResponse
+
+from django.conf import settings
+DEBUG = os.environ.get('DEBUG', 'on') ==  'on'
+SECRET_KEY = os.environ.get('SECRET_KEY', '{{ secret_key }}')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+
+settings.configure(
+    DEBUG=DEBUG,
+    SECRET_KEY=SECRET_KEY,
+    ALLOWED_HOSTS=ALLOWED_HOSTS,
+    ROOT_URLCONF=__name__,
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    ),
+)
+
+def index(request):
+    return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
