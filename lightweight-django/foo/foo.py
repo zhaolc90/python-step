728dfe2866374fe73e8a9fdbb1f2878371129ce1
c <ytmssr@outlook.com>
2018-12-04 22:32:39 +0800

    text

--- lightweight-django/foo/foo.py
+++ lightweight-django/foo/foo.py
@@ -2,7 +2,7 @@
 import sys
 import os
 from io import BytesIO
-from PIL import Image
+from PIL import Image,ImageDraw
 
 from django.conf.urls import url
 from django.core.wsgi import get_wsgi_application
@@ -31,6 +31,13 @@ class ImageForm(forms.Form):
         height = self.cleaned_data['height']
         width = self.cleaned_data['width']
         image=Image.new('RGB', (width,height))
+        draw = ImageDraw.Draw(image)
+        text = '{} X {}'.format(width, height)
+        textwidth, textheight = draw.textsize(text)
+        if textwidth < width and textheight < height:
+            texttop = (height - textheight) // 2
+            textleft = (width - textwidth) // 2
+            draw.text((textleft, texttop), text, fill=(255, 255, 255))
         content = BytesIO()
         image.save(content, image_format)
         content.seek(0)
