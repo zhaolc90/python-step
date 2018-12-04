41e32311b0130a78dfa8e5974b9d66f8f9ec2132
c <ytmssr@outlook.com>
2018-12-04 19:53:19 +0800

    注册一个admin 字段

--- django-demo/mysite/polls/models.py
+++ django-demo/mysite/polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name  = models.CharField(max_length=30)
+    last_name  = models.CharField(max_length=30)
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
