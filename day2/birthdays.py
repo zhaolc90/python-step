3ef698cbe8e5e7468e76abbc123d01dc0c816a67
cln <cln@servyou.com.cn>
2019-02-19 14:36:17 +0800

    change

--- day2/birthdays.py
+++ day2/birthdays.py
@@ -4,7 +4,7 @@ while True:
     print('Enter a name:(blank to quit)')
     name = input()
     if name == '':
-        beak
+        break
 
     if name in birthdays:
         print(birthdays[name] + 'is the birthday of '+name)
